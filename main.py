01e1f71bd0771e6fa454ff97368a746b6ad029a7
OmetovVlad <ometov96@mail.ru>
2023-10-06 19:44:38 +0700

    move to base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 # Получаем массив всех прокси proxy-bunker
 def get_proxy_list():
     response = requests.get('https://proxy-bunker.com/api4.php?&socks=1')
@@ -26,9 +27,11 @@ def config_gen():
 
     proxy_list = get_proxy_list()
 
-    f = open('3proxy.cfg', 'w')
+    proxy_list_file = open('proxy_list.txt', 'w')
 
-    f.write('auth none\n')
+    config_file = open('3proxy.cfg', 'w')
+
+    config_file.write('auth none\n')
 
     i = 0
     for proxy in proxy_list:
@@ -37,18 +40,29 @@ def config_gen():
         if proxy[0] != "":
             current_proxy = proxy[0].split(':')
 
-            if i > 0:
-                f.write('flush\n')
+            proxy_list_file.write(server_ip + ':' + str(server_port) + '|' + proxy[1] + '\n')
 
-            f.write('allow *\n')
-            f.write('parent 1000 http ' + current_proxy[0] + ' ' + current_proxy[1] + '\n')
-            f.write('proxy -n -p' + str(server_port) + '\n\n')
+            if i > 0:
+                config_file.write('flush\n')
+
+            config_file.write('allow *\n')
+            config_file.write('parent 1000 http ' + current_proxy[0] + ' ' + current_proxy[1] + '\n')
+            config_file.write('proxy -n -p' + str(server_port) + '\n\n')
 
             server_port += 1
             i += 1
 
-    return True
-
 
 # Получаем и записываем в файл список всех прокси
 config_gen()
+
+url = 'https://прокси-россии.рф/tasks/addNewProxy/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+fp = open('proxy_list.txt', 'rb')
+
+files = {'file': fp}
+
+resp = requests.post(url, files=files, headers=headers)
+fp.close()
+print(resp.text)
